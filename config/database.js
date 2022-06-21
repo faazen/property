d6f087c351510d97605b9ad989080c4f7dfe4823
faazen <97652528+faazen@users.noreply.github.com>
2022-06-21 12:53:31 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 ////////////////////////////Postavke baze podataka///////////////////////////////
 
 module.exports={
-    database:'mongodb://localhost:27017/Apartments',//url adresa baze
+    database:'mongodb://userpro:passpro@192.168.3.182:27017/dbpro',//url adresa baze
     secret:'mojatajna'  //tajni kod koji passport koristi za kreiranje tokena
     //obavezno mora sadrzati imena casopisa kao sto su: moja tajna, moja sudbina... 
 }
